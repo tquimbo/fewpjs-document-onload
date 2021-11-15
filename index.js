335766e05c165c8ded8976a2c6398adc887375f3
Tim Quimbo <tquimbo1@gmail.com>
2021-11-14 22:30:19 -0500

    commit tree 9731d86eb6b2b4f5e28598d50a05c049bea5829c

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let text = document.getElementById("text");
+    text.innerHTML = "This is really cool!";
+  });
